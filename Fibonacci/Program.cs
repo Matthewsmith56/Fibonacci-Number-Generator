7676619fd4a0f3e99122e518d6ffe98551917e4e
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-09 15:49:58 -0400

    Final Program

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,6 +10,28 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+                long sum = 0;
+                long num1 = 0;
+                long num2 = 1;
+
+            for (int i = 1; i < 100; i++)
+            {
+                long endNum = num1 + num2;
+                Console.WriteLine(endNum);
+
+                num1 = num2;
+                num2 = endNum;
+
+                if (endNum < 4000000 && endNum % 2 == 0)
+                {
+                    sum += endNum;
+                }
+                else if (endNum > 4000000)
+                {
+                    Console.WriteLine("The sum is "+sum);
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
